91533f4fc128936ddbfb489e786a890fa68e9057
SorSaffari <sor.saffari@gmail.com>
2018-10-09 17:01:30 +0200

    update the schema: alignments have midline n identifier. gi is removed

--- blast/blast.py
+++ blast/blast.py
@@ -1,10 +1,9 @@
 import grakn
 import re
-import sys
 import Bio
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
-from util import insert_if_non_existent
+from util import insert_if_non_existent, insert_anyway
 
 
 '''
@@ -14,24 +13,24 @@ from util import insert_if_non_existent
 
 
 def query_target_sequences(session):
-    with session.transaction(grakn.TxType.READ) as tx:
-        q_match_target_sequences = 'match $p isa protein has sequence $s; limit 1; get $s;'
-        print("Extracting target sequences: ", q_match_target_sequences)
-        print("- - - - - - - - - - - - - - - - - - - - -")
-        answers = tx.query(q_match_target_sequences).collect_concepts()
-        sequences = [answer.value() for answer in answers]
-        return sequences
+    # with session.transaction(grakn.TxType.READ) as tx:
+    #     q_match_target_sequences = 'match $p isa protein has sequence $s; limit 1; get $s;'
+    #     print("Extracting target sequences: ", q_match_target_sequences)
+    #     print("- - - - - - - - - - - - - - - - - - - - -")
+    #     answers = tx.query(q_match_target_sequences).collect_concepts()
+    #     sequences = [answer.value() for answer in answers]
+        # return sequences
+    return ["MNVGTAHSEVNPNTRVMNSRGIWLSYVLAIGLLHIVLLSIPFVSVPVVWTLTNLIHNMGMYIFLHTVKGTPFETPDQGKARLLTHWEQMDYGVQFTASRKFLTITPIVLYFLTSFYTKYDQIHFVLNTVSLMSVLIPKLPQLHGVRIFGINKY"]
 
 
 '''
     1. inserts the databse against which the BLAST search ran
     2. for each alignment found, inserts the:
         a. protein entity
-        b. sequence attribute and its corresponding gi attribute
-        c. sequence <> database relationship
-        d. sequence <> sequence alignment relationship
-        e. species entity
-        f. species <> protein relationship
+        b. sequence <> database relationship
+        c. sequence <> sequence alignment relationship
+        d. species entity
+        e. species <> protein relationship
     :param session as dict: used to talk to the proteins keyspace
     :param target_sequence as string: the sequence whose alignments are returned from BLAST
     :param record as BLAST.Record (from Biopython): the BLAST record that contains the result from the BLAST search
@@ -47,11 +46,8 @@ def insert_new_proteins_n_alignments(session, target_sequence, record):
                                          "$db")
 
     for alignment in record.alignments:
-        title_line = alignment.title
-
         # insert the protein entity (if it doesn't already exist)
-        protein_name = title_line.split(" ", 1)[1].split(
-            "[")[0].split(">")[0].replace(";", " -")
+        protein_name = alignment.hit_def
         q_insert_protein = 'insert $pr isa protein has name "' + protein_name + '";'
         protein_id = insert_if_non_existent(session,
                                             q_insert_protein,
@@ -65,14 +61,6 @@ def insert_new_proteins_n_alignments(session, target_sequence, record):
                                    q_insert_sequence_for_protein,
                                    "$seq")
 
-            # insert the gi attribute for the sequence attribute (if doesn't exists already)
-            gi = title_line.split(" ", 1)[0].split("|")[1]
-            q_insert_gi_for_sequence = ('match $seq isa sequence "' + hsp.sbjct + '"; ' +
-                                        'insert $gi isa gi "' + gi + '"; $seq has gi $gi;')
-            insert_if_non_existent(session,
-                                   q_insert_gi_for_sequence,
-                                   "$gi")
-
             # insert the sourcing-of-information relationship (if it doesn't already exist)
             q_insert_sourcing = ('match $seq isa sequence "' + hsp.sbjct + '"; ' +
                                  '$db id ' + database_id + '; ' +
@@ -82,23 +70,26 @@ def insert_new_proteins_n_alignments(session, target_sequence, record):
                                    "$sourcing")
 
             # insert the alignment relationship (if it doesn't already exist)
-            sequence_positivity = (hsp.positives / alignment.length)
-            sequence_identicality = (hsp.identities / alignment.length)
-            sequence_gaps = hsp.gaps / alignment.length
+            sequence_positivity = round(hsp.positives / alignment.length, 3)
+            sequence_identicality = round(hsp.identities / alignment.length, 3)
+            sequence_gaps = round(hsp.gaps / alignment.length, 5)
+            sequence_midline = hsp.match
+            alignment_identifier = alignment.hit_id.split("|", 4)[3]
             q_insert_alignment = ('match $target-seq isa sequence "' + target_sequence + '"; ' +
                                   '$matched-seq isa sequence "' + hsp.sbjct + '"; ' +
                                   'insert $alignment (target-sequence: $target-seq, matched-sequence: $matched-seq) isa sequence-sequence-alignment;')
             q_insert_alignment_attr = ('$alignment has sequence-positivity ' + str(sequence_positivity) +
                                        ' has sequence-identicality ' + str(sequence_identicality) +
-                                       ' has sequence-gaps ' + str(sequence_gaps) + ';')
+                                       ' has sequence-gaps ' + str(sequence_gaps) +
+                                       ' has sequence-midline "' + sequence_midline + '"' +
+                                       ' has identifier "' + alignment_identifier + '";')
             insert_if_non_existent(session,
                                    q_insert_alignment,
                                    q_insert_alignment_attr)
 
         # insert the species entity (if it doesn't already exist)
-        print(title_line)
-        if (len(title_line.split(" ", 1)[1].split("[")) > 1):
-            species = title_line.split(" ", 1)[1].split("[")[1].split("]")[0]
+        if (len(alignment.hit_def.split("[")) > 1):
+            species = alignment.hit_def.split("[")[1][:-1]
             q_insert_species = 'insert $species isa species has name "' + species + '"; '
             species_id = insert_if_non_existent(session,
                                                 q_insert_species,
@@ -207,10 +198,7 @@ with client.session(keyspace="proteins") as session:
         print("Inserting BLAST results into the proteins knowledge graph.")
         print("- - - - - - - - - - - - - - - - - - - - -")
         insert_new_proteins_n_alignments(session, sequence, blast_record)
+
     print("Checking for duplicate proteins ...")
     print("- - - - - - - - - - - - - - - - - - - - -")
     clean_data(session)
-    # with session.transaction(grakn.TxType.READ) as read_tx:
-    #     query = 'match $pr isa protein has sequence "a"; get;'
-    #     answers = read_tx.query(query).collect_concepts()
-    #     print(answers[0].id)
