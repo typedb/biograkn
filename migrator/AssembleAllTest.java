cc13aa3b1655b4f5a75431cd122a81735379a579
syed <syed@grakn.ai>
2019-05-10 18:07:29 +0100

    disable rbe for test

---
+++ migrator/AssembleAllTest.java
@@ -0,0 +1,44 @@
+package grakn.biograkn.migrator;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static grakn.biograkn.precisionmedicine.migrator.Migrator.migratePrecisionMedicine;
+
+import grakn.client.GraknClient;
+import graql.lang.Graql;
+import java.util.List;
+import grakn.core.concept.answer.ConceptMap;
+import graql.lang.query.GraqlCompute;
+
+import grakn.core.concept.answer.Numeric;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class AssembleAllTest {
+
+    @Test
+    public void exampleTest() {
+        assertEquals("migrator/test", "migrator/test");
+    }
+
+    // @Before
+    // public void before() {
+    //     migratePrecisionMedicine("all");
+    // }
+
+    // @Test
+    // public void assembleAllTest() {
+    //     GraknClient graknClient = new GraknClient("127.0.0.1:48555");
+    //     GraknClient.Session session = graknClient.session("precision_medicine");
+
+    //     GraknClient.Transaction readTransaction = session.transaction().read();
+
+    //     GraqlCompute.Statistics query = Graql.compute().count().in("gene");
+
+    //     List<Numeric> insertedIds = readTransaction.execute(query);
+
+    //     System.out.println(insertedIds);
+    // }
+}
