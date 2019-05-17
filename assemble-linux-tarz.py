0e67827e64eac4e47d7b712569e1e134062f10a2
syed <syed@grakn.ai>
2019-05-17 13:50:57 +0100

    delpoy empty grakn snapshot image

--- assemble-linux-tarz.py
+++ assemble-linux-tarz.py
@@ -9,7 +9,7 @@ sp.check_call(["nohup", "grakn-core-all-linux/grakn", "server", "start"])
 
 migrator = migrator.split(" ")
 
-sp.check_call([migrator[0]])
+# sp.check_call([migrator[0]])
 
 sp.check_call(["grakn-core-all-linux/grakn", "server", "stop"])
 
