9e767a5b0d518076b071b58e2d045d31560ef391
syed <syed@grakn.ai>
2019-06-11 13:41:45 +0100

    run deploy github script

--- deploy-github.py
+++ deploy-github.py
@@ -1,4 +1,15 @@
 import subprocess as sp
-import sys
 
+credential = os.getenv('GCP_CREDENTIAL')
+project = 'grakn-dev'
 
+lprint('Configure the gcloud CLI')
+credential_file = '/tmp/gcp-credential.json'
+with open(credential_file, 'w') as f:
+    f.write(credential)
+
+sp.check_call(['gcloud', 'auth', 'activate-service-account', '--key-file', credential_file])
+sp.check_call(['gcloud', 'config', 'set', 'project', project])
+
+sp.check_call(['gsutil', 'defacl', 'ch', '-u', 'AllUsers:READER', 'gs://biograkn'])
+sp.check_call(['gsutil', 'rsync', '-R', './biograkn/dist/', 'gs://biograkn'])
