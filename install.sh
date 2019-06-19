305bfda69f8785c0331f74ff350472c3b94c2cd6
syed <syed@grakn.ai>
2019-06-19 17:38:22 +0100

    install java in biograkn image to run grakn

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ BIOGRAKN_DISTRIBUTION="biograkn-linux"
 tar -xf /tmp/deployment/"$BIOGRAKN_DISTRIBUTION".tar.gz -C /tmp/deployment && \
     cp -r /tmp/deployment/dist/"$BIOGRAKN_DISTRIBUTION" /opt/biograkn
 
+sudo apt install openjdk-8-jdk
+
 mkdir /opt/c2d && \
     cp /tmp/deployment/startup-script.sh /opt/c2d/ && \
     chmod +x /opt/c2d/startup-script.sh && \
