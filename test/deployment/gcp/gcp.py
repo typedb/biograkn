e12dc10f359bdd7da7be3c0a9c9c915abfcec055
syed <syed@grakn.ai>
2019-06-19 13:21:43 +0100

    run test-depoyment-gcp job with gcp.py script

--- test/deployment/gcp/gcp.py
+++ test/deployment/gcp/gcp.py
@@ -1,278 +1,121 @@
 #!/usr/bin/env python
 
-import collections
-import glob
-import json
-import logging
 import os
-import re
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
-import paramiko
-import platform
 import subprocess as sp
-import tempfile
-import time
-
-GCPInstance = collections.namedtuple('GCPInstance',
-                                     ['name', 'zone', 'public_ip', 'password'])
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    format='[%(asctime)s.%(msecs)03d]: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-)
-logger.level = logging.DEBUG
-
-TIMEOUT = 60 * 20  # 20 minutes
-
-ENV_VARIABLES = [
-    'AWS_SSH_KEY_PAIR',
-    'CIRCLE_BUILD_NUM',
-    'CIRCLE_JOB',
-    'CIRCLE_PROJECT_REPONAME',
-    'GCP_DEPLOY_KEY',
-]
-
-SSH_MAX_CONNECT_ATTEMPTS = 5
 
 
-def verify_environment():
-    logger.debug('Verifying environment variables to be present: %s',
-                 ENV_VARIABLES)
-    for var in ENV_VARIABLES:
-        if not os.getenv(var):
-            raise ValueError('Should specify {} env variable'.format(var))
+def lprint(msg):
+    # TODO: replace with proper logging
+    from datetime import datetime
+    print('[{}]: {}'.format(datetime.now().isoformat(), msg))
 
 
-def write_ssh_key():
-    key_file = tempfile.NamedTemporaryFile(delete=False)
-    with open(key_file.name, 'w') as kf:
-        key = os.getenv('AWS_SSH_KEY_PAIR')
-        # private key string stored in env variable is not
-        # parsed correctly by paramiko
-        # specifically, headers should be on separate lines
-        key = key.replace('-----BEGIN RSA PRIVATE KEY----- ', '')
-        key = key.replace(' -----END RSA PRIVATE KEY-----', '')
-        kf.writelines([
-            '-----BEGIN RSA PRIVATE KEY-----\n',
-            key + '\n',
-            '-----END RSA PRIVATE KEY-----\n'
-        ])
-    return key_file.name
+def gcloud_instances_create(instance):
+    sp.check_call([
+        'gcloud',
+        'compute',
+        'instances',
+        'create',
+        'instance',
+        '--image',
+        'grakn-biograkn-snapshot-test',
+        '--machine-type',
+        'n1-standard-4',
+        '--zone=europe-west1-b',
+        '--project=grakn-dev'
+    ])
 
 
-def get_deployment_name():
-    return 'circleci-{}-{}-{}'.format(
-        os.getenv('CIRCLE_PROJECT_REPONAME'),
-        os.getenv('CIRCLE_JOB'),
-        os.getenv('CIRCLE_BUILD_NUM')
-    )
+def gcloud_ssh(instance, command):
+    sp.check_call([
+        'gcloud',
+        'compute',
+        'ssh',
+        instance,
+        '--command=' + command,
+        '--zone=europe-west1-b',
+        '--project=grakn-dev'
+    ])
+
+def gcloud_scp(instance, local, remote):
+    sp.check_call([
+        'gcloud',
+        'compute',
+        'scp',
+        local,
+        instance + ':' + remote,
+        '--zone=europe-west1-b',
+        '--project=grakn-dev'
+    ])
 
 
-# TODO(vmax): now script depends on invocation folder
-class GCPClient:
-    def __init__(self, gcp_credential, key_file):
-        credential = service_account.Credentials.from_service_account_info(
-            json.loads(gcp_credential),
-            scopes=['https://www.googleapis.com/auth/cloud-platform'])
-
-        self._deploymentmanager = build('deploymentmanager',
-                                        'v2',
-                                        credentials=credential,
-                                        cache_discovery=False)
-        self._compute = build('compute', 'v1', credentials=credential, cache_discovery=False)
-        self._key = paramiko.RSAKey.from_private_key_file(key_file)
-        self._ssh_client_cache = {}
-
-    def _read_imports(self):
-        files = {
-            os.path.basename(x): open(x).read()
-            for x in glob.glob('./deployment/deploy-gcp-image/deploy/**/*.*') +
-                     glob.glob('./deployment/deploy-gcp-image/deploy/grakn.jinja*')
-        }
-
-        files['grakn.jinja'] = files['grakn.jinja'].replace(
-            # embed SSH public key
-            '# {SSH_KEYS_PLACEHOLDER}',
-            '        - key: ssh-keys\n          value: "{}"'.format(self._ssh_public_key())
-        ).replace(
-            # embed source image name
-            '# {SOURCE_IMAGE_PLACEHOLDER}',
-            self._get_image_name()
-        ).replace(
-            # allow inbound access on Grakn port
-            'properties["enableTcp48555"]',
-            'true'
-        )
-
-        return [{'name': k, 'content': v} for k, v in files.items()]
-
-    def _ssh_public_key(self):
-        return 'circleci:ssh-rsa {pubkey} circleci'.format(
-            pubkey=self._key.get_base64())
-
-    def _get_image_name(self):
-        commit = sp.check_output(['git', 'rev-parse', 'HEAD']).strip()
-        return 'grakn-kgms-snapshot-{}'.format(commit)
-
-    def _wait_for_operation(self, operation):
-        while True:
-            result = self._deploymentmanager.operations().get(
-                project='grakn-dev', operation=operation).execute()
-
-            if result['status'] == 'DONE':
-                logger.debug("[operation '%s']: done", operation)
-                if 'error' in result:
-                    logger.error("[operation '%s']: error", operation)
-                    raise Exception(result['error'])
-                return result
-
-            time.sleep(1)
-
-    def _get_public_ip(self, name, zone):
-        response = self._compute.instances().list(
-            project='grakn-dev', zone=zone,
-            filter='name={}'.format(name)).execute()
-        if len(response['items']) == 1:
-            return response['items'][0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']
-
-    def ssh_session(self, instance):
-        import paramiko
-        if instance in self._ssh_client_cache:
-            return self._ssh_client_cache[instance]
-
-        attempts = SSH_MAX_CONNECT_ATTEMPTS
-        while attempts:
-            _client = paramiko.client.SSHClient()
-            _client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-            try:
-                _client.connect(instance.public_ip, username='circleci', pkey=self._key)
-                self._ssh_client_cache[instance] = _client
-                return _client
-            except paramiko.ssh_exception.NoValidConnectionsError:
-                attempts -= 1
-                logger.debug('Could not connect; will attempt %s more times', attempts)
-                time.sleep(15)
-        raise Exception('Could not connect after %s attempts', SSH_MAX_CONNECT_ATTEMPTS)
-
-    def is_node_ready(self, instance):
-        client = self.ssh_session(instance)
-        _, stdout, _ = client.exec_command(
-            "grep --quiet 'Deployment startup finished.' /var/log/grakn/cluster.log"
-        )
-        while not stdout.channel.exit_status_ready():
-            # wait until exit code is ready
-            pass
-
-        return stdout.channel.exit_status == 0
-
-    def create_deployment(self, name):
-        config = {
-            'name': name,
-            'target': {
-                'config': {
-                    'content':
-                        open(
-                            './deployment/deploy-gcp-image/deploy/test_config.yaml'
-                        ).read()
-                },
-                'imports': self._read_imports()
-            },
-        }
-        operation = self._deploymentmanager.deployments().insert(
-            body=config, project='grakn-dev').execute()
-        self._wait_for_operation(operation['name'])
-
-    def delete_deployment(self, name):
-        operation = self._deploymentmanager.deployments().delete(
-            project='grakn-dev', deployment=name).execute()
-        self._wait_for_operation(operation['name'])
-
-    def instances_in_deployment(self, name):
-        import yaml
-        instances = []
-        zone_regex = re.compile('.*zones/(?P<zone>[^/]*)/.*')
-        response = self._deploymentmanager.resources().list(
-            project='grakn-dev', deployment=name).execute()
-        for i in response['resources']:
-            if i['type'] == 'compute.v1.instance':
-                name = i['name']
-                zone = re.search(zone_regex, i['url']).group('zone')
-                password = None
-                metadata = yaml.safe_load(i['finalProperties'])
-                for kv in metadata['metadata']['items']:
-                    if kv['key'] == 'STORAGE_USER_PWD':
-                        password = kv['value']
-                instances.append(
-                    GCPInstance(name, zone, self._get_public_ip(name, zone),
-                                password))
-        return instances
+def gcloud_scp_from_remote(instance, remote, local):
+    sp.call([
+        'gcloud',
+        'compute',
+        'scp',
+        instance + ':' + remote,
+        local,
+        '--zone=europe-west1-b',
+        '--project=grakn-dev'
+    ])
 
 
-if __name__ == "__main__":
-    client = None
-    deployment_name = get_deployment_name()
-    try:
-        verify_environment()
+def gcloud_instances_delete(instance):
+    sp.check_call([
+        'gcloud',
+        '--quiet',
+        'compute',
+        'instances',
+        'delete',
+        instance,
+        '--delete-disks=all',
+        '--zone=europe-west1-b'
+    ])
 
-        logger.debug('Creating Google Compute Platform client')
-        client = GCPClient(os.getenv('GCP_DEPLOY_KEY'), write_ssh_key())
 
-        logger.debug('Creating deployment [%s]', deployment_name)
-        client.create_deployment(deployment_name)
+# TODO: exit if CIRCLE_BUILD_NUM and $GCP_CREDENTIAL aren't present
+credential = os.getenv('GCP_CREDENTIAL')
+project = 'grakn-dev'
+instance = 'circleci-' + os.getenv('CIRCLE_PROJECT_REPONAME') + '-' + os.getenv('CIRCLE_JOB') + '-' + os.getenv('CIRCLE_BUILD_NUM')
 
-        instances = client.instances_in_deployment(deployment_name)
-        logger.debug('Instances: %s', instances)
+try:
+    lprint('Configure the gcloud CLI')
+    credential_file = '/tmp/gcp-credential.json'
+    with open(credential_file, 'w') as f:
+        f.write(credential)
+    sp.check_call(['gcloud', 'auth', 'activate-service-account', '--key-file', credential_file])
+    sp.check_call(['gcloud', 'config', 'set', 'project', project])
+    sp.check_call(['ssh-keygen', '-t', 'rsa', '-b', '4096', '-N', '', '-f', os.path.expanduser('~/.ssh/google_compute_engine')])
 
-        logger.debug('Waiting until all nodes are up...')
-        start_time = time.time()
+    lprint('Creating a CentOS instance "' + instance + '"')
+    gcloud_instances_create(instance)
 
-        all_nodes = set(instances)
-        ready_nodes = set()
+    # lprint('Installing dependencies')
+    # gcloud_ssh(instance, 'sudo yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm')
+    # gcloud_ssh(instance, 'sudo yum update -y')
+    # gcloud_ssh(instance, 'sudo yum install -y sudo procps which gcc gcc-c++ python-devel unzip git java-1.8.0-openjdk-devel rpm-build yum-utils')
+    # gcloud_ssh(instance, 'curl -OL https://github.com/bazelbuild/bazel/releases/download/0.20.0/bazel-0.20.0-installer-linux-x86_64.sh')
+    # gcloud_ssh(instance, 'chmod +x bazel-0.20.0-installer-linux-x86_64.sh')
+    # gcloud_ssh(instance, 'sudo ./bazel-0.20.0-installer-linux-x86_64.sh')
 
-        while len(ready_nodes) < len(
-                all_nodes) and time.time() - start_time < TIMEOUT:
-            for node in all_nodes - ready_nodes:
-                if client.is_node_ready(node):
-                    ready_nodes.add(node)
-                    logger.debug('[%d/%d] Node with name %s is up',
-                                 len(ready_nodes), len(all_nodes), node.name)
-                time.sleep(15)
+    lprint('Copying grakn distribution from CircleCI job into "' + instance + '"')
 
-        if ready_nodes != all_nodes:
-            raise Exception(
-                'Not all nodes came up on time, expected {}, got {}'.format(
-                    all_nodes, ready_nodes))
+    sp.check_call(['cat', 'VERSION'])
+    sp.check_call(['zip', '-r', 'grakn.zip', '.'])
+    gcloud_scp(instance, local='grakn.zip', remote='~')
+    gcloud_ssh(instance, 'unzip grakn.zip')
 
-        num_instances = len(instances)
-        for i, instance in enumerate(instances, start=1):
-            logger.debug('Testing instance %d/%d', i, num_instances)
-            logger.debug('Instance public IP: %s', instance.public_ip)
+    lprint('Installing RPM packages. Grakn will be available system-wide')
+    gcloud_ssh(instance, 'sudo yum-config-manager --add-repo https://repo.grakn.ai/repository/meta/rpm-snapshot.repo')
+    gcloud_ssh(instance, 'sudo yum -y update')
+    gcloud_ssh(instance, 'sudo yum -y install grakn-core-all-$(cat VERSION)')
 
-            env = dict(os.environ)
-            env.update({
-                'KGMS_APPLICATION_TEST_ADDRESS': '{}:48555'.format(instance.public_ip),
-                'KGMS_APPLICATION_TEST_USERNAME': 'grakn',
-                'KGMS_APPLICATION_TEST_PASSWORD': instance.password,
-            })
-
-            logger.debug(
-                'Running bazel run //test/common:kgms-application-test')
-            start_time = time.time()
-            sp.check_call([
-                'bazel',
-                'run',
-                '@graknlabs_build_tools//ci:run-bazel-rbe',
-                '--',
-                'bazel',
-                'run',
-                '--',
-                '//test/common:kgms-application-test',
-            ], env=env)
-            logger.debug('Took %0.2f seconds', time.time() - start_time)
-    finally:
-        logger.debug('Removing deployment [%s]', deployment_name)
-        if client:
-            client.delete_deployment(deployment_name)
+    gcloud_ssh(instance, 'grakn server start')
+    gcloud_ssh(instance, 'grakn server stop')
+finally:
+    lprint('Copying logs from CentOS instance')
+    gcloud_scp_from_remote(instance, remote='/opt/grakn/core/logs/grakn.log', local='./grakn.log')
+    gcloud_scp_from_remote(instance, remote='/opt/grakn/core/logs/cassandra.log', local='./cassandra.log')
+    lprint('Deleting the CentOS instance')
+    gcloud_instances_delete(instance)
