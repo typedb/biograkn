ee0004606723f23aefdefff487e4f58f91bc37bd
syed <syed@grakn.ai>
2019-06-20 11:25:06 +0100

    invoke startup script

--- deploy-gcp-snapshot.py
+++ deploy-gcp-snapshot.py
@@ -3,6 +3,7 @@
 import subprocess as sp
 import shutil
 import os
+from pwd import getpwnam
 
 sp.check_call(['curl', '-L', 'https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip', '-o', 'packer.zip'])
 
@@ -11,6 +12,22 @@ sp.check_call(['unzip', 'packer.zip'])
 shutil.copy('./install.sh', './dist')
 shutil.copy('./shutdown-script.sh', './dist')
 shutil.copy('./startup-script.sh', './dist')
+
+
+def filecopy(src, dest, owner=None, mode=None):
+    shutil.copy(src, dest)
+    if owner:
+        owner_pwnam = getpwnam(owner)
+        os.chown(dest, owner_pwnam.pw_uid, owner_pwnam.pw_gid)
+    if mode:
+        os.chmod(dest, mode)
+
+filecopy(
+    os.path.join('/tmp/deployment/', 'rc.local'),
+    '/etc/rc.local',
+    'root',
+    0o755
+)
 
 sp.check_call([
     './packer',
