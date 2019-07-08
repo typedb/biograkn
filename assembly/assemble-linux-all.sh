a906eaaf02499a3a9aa2d132497fc1fe089cc6ab
syed <syed@grakn.ai>
2019-07-08 15:41:37 +0100

    cd into biograkn for every command after cloning

--- assembly/assemble-linux-all.sh
+++ assembly/assemble-linux-all.sh
@@ -3,9 +3,6 @@
 INSTANCE_NAME="assemble-biograkn-linux"
 ZONE="us-east1-b"
 
-mkdir ~/logs
-LOG=~/logs/launch_executor_$INSTANCE_NAME.log
-
 
 export GCP_ACCOUNT_FILE=$(mktemp)
 echo $GCP_DEPLOY_KEY_SNAPSHOT >> $GCP_ACCOUNT_FILE
@@ -14,7 +11,7 @@ gcloud auth activate-service-account --key-file $GCP_ACCOUNT_FILE
 gcloud config set project 'grakn-dev'
 
 
-echo "Creating google cloud compute instance $INSTANCE_NAME..." | tee -a $LOG
+echo "Creating google cloud compute instance $INSTANCE_NAME..."
 
 #TODO: update metadata for project ssh keys
 #for now it will keep trying to Updating project ssh metadata... and fail every time we try to ssh in it
@@ -26,12 +23,12 @@ gcloud compute instances create $INSTANCE_NAME     \
     2>&1 | tee -a $LOG
 
 
-echo "Waiting for $INSTANCE_NAME to be up and running..." | tee -a $LOG
+echo "Waiting for $INSTANCE_NAME to be up and running..."
 
 RET=1
 while [ $RET -ne 0 ]; do
     sleep 1;
-    gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='true' 2>&1 | tee -a $LOG
+    gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='true'
     RET=$?; # collect return code
 done
 
@@ -58,52 +55,51 @@ echo "lalallala"
 
 gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="ps aux | grep -i apt"
 
-
-
-
-
 echo "Installing git..."
 gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git'
 
-#echo "Downloading git-lfs..."
-#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash'
-#
-#echo "Installing git-lfs..."
-#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git-lfs'
-#
-#echo "Installing lfs..."
-#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='git lfs install --skip-smudge'
+echo "Downloading git-lfs..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash'
+
+echo "Installing git-lfs..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git-lfs'
+
+echo "Installing lfs..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='git lfs install'
 
 echo "Cloning BioGrakn..."
 gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="git clone https://github.com/graknlabs/biograkn"
 
-echo "cd biograkn..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd /home/ubuntu/biograkn/"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="ls -la"
 
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="git lfs pull"
+
+echo "cd biograkn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/"
+
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && git lfs pull"
 
 
 
 echo "Building Grakn core..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel build @graknlabs_grakn_core//:assemble-linux-targz"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && bazel build @graknlabs_grakn_core//:assemble-linux-targz"
 
 echo "Extracting Grakn core..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mkdir dist && tar -xvzf bazel-genfiles/external/graknlabs_grakn_core/grakn-core-all-linux.tar.gz -C dist/"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && mkdir dist && tar -xvzf bazel-genfiles/external/graknlabs_grakn_core/grakn-core-all-linux.tar.gz -C dist/"
 
 echo "Starting Grakn..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="nohup dist/grakn-core-all-linux/grakn server start"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && nohup dist/grakn-core-all-linux/grakn server start"
 
 echo "Migrating BioGrakn..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel run //migrator:migrator-bin"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && bazel run //migrator:migrator-bin"
 
 echo "Stoping Grakn..."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="dist/grakn-core-all-linux/grakn server stop"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && dist/grakn-core-all-linux/grakn server stop"
 
 echo "Renaming.."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mv dist/grakn-core-all-linux dist/biograkn-linux"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && mv dist/grakn-core-all-linux dist/biograkn-linux"
 
 echo "Taring BioGrakn.."
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="tar -czf dist/biograkn-linux.tar.gz dist/biograkn-linux"
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn/ && tar -czf dist/biograkn-linux.tar.gz dist/biograkn-linux"
 
 echo "Moving BioGrakn to ciclec ci.."
 gcloud compute scp --recurse ubuntu@$INSTANCE_NAME:"~/dist" --zone=$ZONE ~/
