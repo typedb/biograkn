96941f575cbeed4cf36153487dff3865bc0a47b8
syed <syed@grakn.ai>
2019-07-08 12:07:28 +0100

    skip installing git lfs

--- assembly/assemble-linux-all.sh
+++ assembly/assemble-linux-all.sh
@@ -7,9 +7,9 @@ mkdir ~/logs
 LOG=~/logs/launch_executor_$INSTANCE_NAME.log
 
 
-export GCP_ACCOUNT_FILE=$(mktemp)
-echo $GCP_DEPLOY_KEY_SNAPSHOT >> $GCP_ACCOUNT_FILE
-gcloud auth activate-service-account --key-file $GCP_ACCOUNT_FILE
+#export GCP_ACCOUNT_FILE=$(mktemp)
+#echo $GCP_DEPLOY_KEY_SNAPSHOT >> $GCP_ACCOUNT_FILE
+gcloud auth activate-service-account --key-file ~/Desktop/gcp_credential.json
 
 gcloud config set project 'grakn-dev'
 
@@ -35,48 +35,75 @@ while [ $RET -ne 0 ]; do
     RET=$?; # collect return code
 done
 
+
 #If the boot hasnt finished apt-get will fail
-echo "Waiting for boot to finish..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='while [[ ! -f /var/lib/cloud/instance/boot-finished ]]; do echo -n . && sleep 2; done;' 2>&1 | tee -a $LOG
+echo "Waiting for boot to finish..."
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='while [[ ! -f /var/lib/cloud/instance/boot-finished ]]; do echo -n . && sleep 2; done;'
 
-echo "Installing git..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git' 2>&1 | tee -a $LOG
 
-echo "Downloading git-lfs..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash' 2>&1 | tee -a $LOG
+#
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='systemctl stop apt-daily.service'
+#
+#
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='systemctl kill --kill-who=all apt-daily.service'
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="while ! (systemctl list-units --all apt-daily.service | egrep -q '(dead|failed)'); do sleep 1; done;"
 
-echo "Installing git-lfs..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git-lfs' 2>&1 | tee -a $LOG
 
-echo "Installing lfs..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='git lfs install' 2>&1 | tee -a $LOG
 
-echo "Cloning BioGrakn..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="git clone https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME" 2>&1 | tee -a $LOG
 
-echo "cd biograkn..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd biograkn" 2>&1 | tee -a $LOG
+sleep 120;
 
-echo "Building Grakn core..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel build @graknlabs_grakn_core//:assemble-linux-targz" 2>&1 | tee -a $LOG
+echo "lalallala"
 
-echo "Extracting Grakn core..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mkdir dist && tar -xvzf bazel-genfiles/external/graknlabs_grakn_core/grakn-core-all-linux.tar.gz -C dist/" 2>&1 | tee -a $LOG
 
-echo "Starting Grakn..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="nohup dist/grakn-core-all-linux/grakn server start" 2>&1 | tee -a $LOG
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="ps aux | grep -i apt"
 
-echo "Migrating BioGrakn..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel run //migrator:migrator-bin" 2>&1 | tee -a $LOG
 
-echo "Stoping Grakn..." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="dist/grakn-core-all-linux/grakn server stop" 2>&1 | tee -a $LOG
 
-echo "Renaming.." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mv dist/grakn-core-all-linux dist/biograkn-linux" 2>&1 | tee -a $LOG
 
-echo "Taring BioGrakn.." | tee -a $LOG
-gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="tar -czf dist/biograkn-linux.tar.gz dist/biograkn-linux" 2>&1 | tee -a $LOG
 
-echo "Moving BioGrakn to ciclec ci.." | tee -a $LOG
+echo "Installing git..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git'
+
+#echo "Downloading git-lfs..."
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash'
+#
+#echo "Installing git-lfs..."
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='sudo apt-get install git-lfs'
+#
+#echo "Installing lfs..."
+#gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command='git lfs install --skip-smudge'
+
+echo "Cloning BioGrakn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="git clone https://github.com/graknlabs/biograkn"
+
+echo "cd biograkn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="cd /home/ubuntu/biograkn/"
+
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="git lfs pull"
+
+
+
+echo "Building Grakn core..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel build @graknlabs_grakn_core//:assemble-linux-targz"
+
+echo "Extracting Grakn core..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mkdir dist && tar -xvzf bazel-genfiles/external/graknlabs_grakn_core/grakn-core-all-linux.tar.gz -C dist/"
+
+echo "Starting Grakn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="nohup dist/grakn-core-all-linux/grakn server start"
+
+echo "Migrating BioGrakn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="bazel run //migrator:migrator-bin"
+
+echo "Stoping Grakn..."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="dist/grakn-core-all-linux/grakn server stop"
+
+echo "Renaming.."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="mv dist/grakn-core-all-linux dist/biograkn-linux"
+
+echo "Taring BioGrakn.."
+gcloud compute ssh ubuntu@$INSTANCE_NAME --zone=$ZONE --command="tar -czf dist/biograkn-linux.tar.gz dist/biograkn-linux"
+
+echo "Moving BioGrakn to ciclec ci.."
 gcloud compute scp --recurse ubuntu@$INSTANCE_NAME:"~/dist" --zone=$ZONE ~/
